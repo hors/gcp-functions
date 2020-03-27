d11d08613f1a31924c3d76286d52c5fa0dbf6b9f
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2020-03-27 12:56:07 +0200

    Init commit

---
+++ cmd/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/compute/v1"
+)
+
+func main() {
+	ctx := context.Background()
+
+	c, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	computeService, err := compute.New(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	project := os.Getenv("GCP_PROJECT")
+	zonesReq := computeService.Zones.List(project)
+	if err := zonesReq.Pages(ctx, func(page *compute.ZoneList) error {
+		for _, zone := range page.Items {
+			disksReq := computeService.Disks.List(project, zone.Name)
+			if err := disksReq.Pages(ctx, func(page *compute.DiskList) error {
+				for _, disk := range page.Items {
+					fmt.Printf("name: %v status: %v zone: %v\n", disk.Name, disk.Status, zone.Name)
+				}
+				return nil
+			}); err != nil {
+				log.Fatal(err)
+			}
+		}
+		return nil
+	}); err != nil {
+		log.Fatal(err)
+	}
+}
